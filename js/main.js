230db8f4aedfc80d808fb400683e2420b1d7e0d0
radow1 <radow12@hanmail.net>
2022-03-30 17:44:32 +0900

    Javascript:JS Transparents

--- js/main.js
+++ js/main.js
@@ -23,6 +23,26 @@ navbarMenu.addEventListener("click", event => {
     return;
   }
   console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  // const scrollTo = document.querySelector(link);
+  scrollIntoviews(link);
 });
+
+// handle click on " contact me" button on home
+const contBtn = document.querySelector(".home__contact");
+contBtn.addEventListener("click", () => {
+  scrollIntoviews("#contact");
+});
+
+// make home slowly fade to tranparent as the window scrolls down
+const home = document.querySelector("#home");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  // console.log(`homeHeight:${homeHeight}`);
+  console.log(`homeHeight:${1 - window.scrollY / homeHeight}`);
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+function scrollIntoviews(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
